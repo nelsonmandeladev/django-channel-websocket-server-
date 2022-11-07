3a9535b0f91558d33adb16cbf8853fc830f7ce3d
Sonfack Nelson Mandea <patricknelson1995@gmail.com>
2022-11-07 11:48:18 +0100

    first commit

---
+++ chat/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, "chat/index.html")
+
+def home(request):
+    return render(request, "chat/home.html")
+
+
+def room(request, room_name):
+    context = {
+        "room_name": room_name
+    }
+    return render(request, "chat/room.html", context=context)
